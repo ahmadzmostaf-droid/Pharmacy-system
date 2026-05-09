d4240f67a095f4c92b25805ae7052df6e8e2b040
Maher Al Moustafa <ahmadzmostaf@gmail.com>
2026-05-09 19:55:50 +0300

    KAN-1: Final setup for insolated Login CI environment

--- app.js
+++
@@ -1,7 +0,0 @@
-function validateLogin(username, password) {
-    if (username === "admin" && password === "123") {
-        return true;
-    }
-    return false;
-}
-module.exports = validateLogin;
--- app.test.js
+++
@@ -1,5 +0,0 @@
-const validateLogin = require('./app');
-
-test('تاكيد عملية تسجيل الدخول للادمن', () => {
-    expect(validateLogin("admin", "123")).toBe(true);
-});
